47e7ec2151fa11422369f5784011c5b48d048871
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2024-03-28 14:01:22 +0400

    first commit

--- index.mjs
+++ index.mjs
@@ -2,31 +2,28 @@ import { readFile, readFileSync } from "node:fs";
 import * as http from "node:http";
 
 const servePage = (url, res) => {
-  if (url.split("/")[2] === "home" || url === "/") {
-    readFile(`pages/index.html`, "utf-8", (err, data) => {
-      res.writeHead(err ? 404 : 200, {
-        "Content-Type": "text/html;charset=utf-8",
-      });
-      res.write(
-        err || url.split("/")[2] === "home"
-          ? readFileSync("pages/404.html")
-          : data
-      );
-      res.end();
+  const getPage = (url) => {
+    return url.split("/")[2] === "home" || url === "/"
+      ? "index.html"
+      : `${url.split("/")[2]}.html`;
+  };
+
+  readFile(`pages/${getPage(url)}`, "utf-8", (err, data) => {
+    res.writeHead(err ? 404 : 200, {
+      "Content-Type": "text/html;charset=utf-8",
     });
-  } else {
-    readFile(`pages/${url.split("/")[2]}.html`, "utf-8", (err, data) => {
-      res.writeHead(err ? 404 : 200, {
-        "Content-Type": "text/html;charset=utf-8",
-      });
-      res.write(err ? readFileSync("pages/404.html") : data);
-      res.end();
-    });
-  }
+    res.write(
+      err || url.split("/")[2] === "index"
+        ? readFileSync("pages/404.html")
+        : data
+    );
+    res.end();
+  });
 };
 
 const app = http.createServer((req, res) => {
   if (req.method === "GET") servePage(req.url, res);
 });
 
-app.listen(4000, () => console.log("Server is running on port 4000"));
+const PORT = 4000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
