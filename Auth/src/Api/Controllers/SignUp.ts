7126604d89b3747b6c71a40ba95b19566acf7e9d
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2024-04-07 12:08:14 +0400

    User model and password hashing

--- Auth/src/Api/Controllers/SignUp.ts
+++ Auth/src/Api/Controllers/SignUp.ts
@@ -1,5 +1,19 @@
 import { Request, Response } from "express";
+import User from "../../Model/UserModel";
 
 export const signUp = async (req: Request, res: Response) => {
-  res.json({ message: "Sign Up route" });
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Email is already in use" });
+    }
+    const user = await User.build({ email, password }).save();
+
+    return res.status(201).json({ user });
+  } catch (error) {
+    console.error(error);
+  }
 };
--- Auth/src/index.ts
+++ Auth/src/index.ts
@@ -1,12 +1,24 @@
 import express from "express";
 import { json } from "body-parser";
 import { router } from "./Api/Routes";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(json());
 
 app.use(router);
 
-app.listen(process.env.AUTH_PORT, () =>
-  console.log(`AUTH SERVICE: LIVE ON ${process.env.AUTH_PORT}`)
-);
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("AUTH DB: CONNECTED");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(process.env.AUTH_PORT, () =>
+    console.log(`AUTH SERVICE: LIVE ON ${process.env.AUTH_PORT}`)
+  );
+};
+
+start();
