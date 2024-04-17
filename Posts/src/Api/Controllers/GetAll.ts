d34099f123f897f7eb68fa3bf907d81128dca6b6
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2024-04-17 21:07:38 +0400

    Post model added

--- Posts/src/Api/Controllers/GetAll.ts
+++ Posts/src/Api/Controllers/GetAll.ts
@@ -1,8 +1,20 @@
 import { Request, Response } from "express";
+import Post from "../../Model/PostModel";
 
 export const getAll = async (req: Request, res: Response) => {
   try {
-    return res.status(200).json({ posts: [] });
+    const dummyData = {
+      userId: "6620007a5cec8869d87f4389",
+      title: "Test Title",
+      context: "Test Context",
+      image: "test/img/path",
+    };
+
+    const newPost = Post.build(dummyData);
+    await newPost.save();
+
+    const posts = await Post.find();
+    return res.status(200).json({ posts });
   } catch (error) {
     console.error(error);
   }
