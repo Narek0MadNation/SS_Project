fa4f2b5f7862d6ad031b6a709c6fb37b829ffc21
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2024-04-22 10:48:10 +0400

    fix

--- PostsComment/src/Api/Routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getComments } from "../controllers/getComments";
-
-const router = Router();
-
-router.get("/:postId", getComments);
-
-export { router };
